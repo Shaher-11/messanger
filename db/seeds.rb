b83e6a92e6cde837e953a42fbc9d342ed6de1f21
Shaher-11 <shahershamroukh@gmail.com>
2021-02-11 10:27:14 +0200

    added devise and user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(email: 'humblebragger@humblebrag.com', password: 'humblebaby')
+User.create(email: 'fitnessgrampacer@test.com', password: 'pacertest')
